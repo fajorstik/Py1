510842195f1eabe6ef2c5b5ab0061bbd452b4617
fajorstik <160943914+fajorstik@users.noreply.github.com>
2024-02-22 20:43:40 +0100

    Add files via upload

---
+++ Py1.py
@@ -0,0 +1,12 @@
+msg = "Roll a dice"
+print(msg)
+print("PY1")
+
+PendingDeprecationWarning
+msg
+msg.capitalize()
+msg.split()
+print("sep:str None = " ", end: str") 
+print("sep:str None = " ", end: file: SupportsWrite(str) None = sep:str " ", end: None = None, flush: Literal(False = False) None")
+SyntaxWarning
+print("file: _SupportsWriteAndFlush(str) None = None=, (flush: bool)_ None")
